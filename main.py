916d9a9d7c26a3a1a4be0444b4453d153f5abb87
sriramabd <55134619+sriramabd@users.noreply.github.com>
2023-03-06 15:31:11 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from wsgiref import simple_server
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask import Response
 import os
 from flask_cors import CORS, cross_origin
@@ -16,14 +15,29 @@ app = Flask(__name__)
 #dashboard.bind(app)
 CORS(app)
 
-
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRouteClient():
     try:
-        if request.json['folderPath'] is not None:
-            path = request.json['folderPath']
+        if request.json is not None:
+            path = request.json['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path = pred.predictionFromModel()
+            return Response("Prediction File created at %s!!!" % path)
+        elif request.form is not None:
+            path = request.form['filepath']
 
             pred_val = pred_validation(path) #object initialization
 
@@ -49,8 +63,8 @@ def predictRouteClient():
 def trainRouteClient():
 
     try:
-        if request.json['folderPath'] is not None:
-            path = request.json['folderPath']
+        if request.json['filepath'] is not None:
+            path = request.json['filepath']
             train_valObj = train_validation(path) #object initialization
 
             train_valObj.train_validation()#calling the training_validation function
@@ -73,10 +87,6 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-#port = int(os.getenv("PORT"))
+port = int(os.getenv("PORT",5001))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    app.run(port=port,debug=True)
