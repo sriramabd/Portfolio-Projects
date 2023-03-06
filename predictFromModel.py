ecdc04188631a3552b41d1820a9d5872a07e1e7c
sriramabd <55134619+sriramabd@users.noreply.github.com>
2023-03-06 15:32:06 -0500

    Add files via upload

--- predictFromModel.py
+++ predictFromModel.py
@@ -1,10 +1,10 @@
-import pandas
+import pandas as pd
+import numpy as np
 from file_operations import file_methods
 from data_preprocessing import preprocessing
 from data_ingestion import data_loader_prediction
 from application_logging import logger
 from Prediction_Raw_Data_Validation.predictionDataValidation import Prediction_Data_validation
-
 
 
 class prediction:
@@ -22,71 +22,44 @@ class prediction:
             data_getter=data_loader_prediction.Data_Getter_Pred(self.file_object,self.log_writer)
             data=data_getter.get_data()
 
-            #code change
-            # wafer_names=data['Wafer']
-            # data=data.drop(labels=['Wafer'],axis=1)
 
-            preprocessor=preprocessing.Preprocessor(self.file_object,self.log_writer)
+            preprocessor = preprocessing.Preprocessor(self.file_object, self.log_writer)
 
-            is_null_present,cols_with_missing_values=preprocessor.is_null_present(data)
-            if(is_null_present):
-                data=preprocessor.impute_missing_values(data)
 
-            #data  = preprocessor.logTransformation(data)
-            cols_to_drop = ["Item_Identifier", "Outlet_Identifier"]
-            data_useful = preprocessor.remove_columns(data, cols_to_drop)
-            #scale the prediction data
-            data_scaled = preprocessor.scale_numerical_columns(data_useful)
+            # check if missing values are present in the dataset
+            is_null_present, cols_with_missing_values = preprocessor.is_null_present(data)
 
-            #data=data.to_numpy()
+            # if missing values are there, replace them appropriately.
+            if (is_null_present):
+                data = preprocessor.impute_missing_values(data, cols_with_missing_values)  # missing value imputation
+
+            # Proceeding with more data pre-processing steps
+            X = preprocessor.scale_numerical_columns(data)
+
+
             file_loader=file_methods.File_Operation(self.file_object,self.log_writer)
             kmeans=file_loader.load_model('KMeans')
 
-            ##Code changed
-            #pred_data = data.drop(['Wafer'],axis=1)
-            clusters=kmeans.predict(data_scaled)#drops the first column for cluster prediction
-            data_scaled['clusters']=clusters
-            clusters=data_scaled['clusters'].unique()
-            result=[] # initialize blank list for storing predicitons
-            # with open('EncoderPickle/enc.pickle', 'rb') as file: #let's load the encoder pickle file to decode the values
-            #     encoder = pickle.load(file)
 
+            clusters=kmeans.predict(X)#drops the first column for cluster prediction
+            X['clusters']=clusters
+            clusters=X['clusters'].unique()
+            predictions=[]
             for i in clusters:
-                cluster_data= data_scaled[data_scaled['clusters']==i]
+                cluster_data= X[X['clusters']==i]
                 cluster_data = cluster_data.drop(['clusters'],axis=1)
                 model_name = file_loader.find_correct_model_file(i)
                 model = file_loader.load_model(model_name)
-                for val in (model.predict(cluster_data.values)):
-                    result.append(val)
-            result = pandas.DataFrame(result,columns=['Predictions'])
-            result['Item_Identifier']  = data["Item_Identifier"]
-            result["Outlet_Identifier"] = data["Outlet_Identifier"]
+                result=(model.predict(cluster_data))
+
+            final= pd.DataFrame(list(zip(result)),columns=['Predictions'])
             path="Prediction_Output_File/Predictions.csv"
-            result.to_csv("Prediction_Output_File/Predictions.csv",header=True) #appends result to prediction file
+            final.to_csv("Prediction_Output_File/Predictions.csv",header=True,mode='a+') #appends result to prediction file
             self.log_writer.log(self.file_object,'End of Prediction')
         except Exception as ex:
             self.log_writer.log(self.file_object, 'Error occured while running the prediction!! Error:: %s' % ex)
             raise ex
         return path
-
-            # old code
-            # i=0
-            # for row in data:
-            #     cluster_number=kmeans.predict([row])
-            #     model_name=file_loader.find_correct_model_file(cluster_number[0])
-            #
-            #     model=file_loader.load_model(model_name)
-            #     #row= sparse.csr_matrix(row)
-            #     result=model.predict([row])
-            #     if (result[0]==-1):
-            #         category='Bad'
-            #     else:
-            #         category='Good'
-            #     self.predictions.write("Wafer-"+ str(wafer_names[i])+','+category+'\n')
-            #     i=i+1
-            #     self.log_writer.log(self.file_object,'The Prediction is :' +str(result))
-            # self.log_writer.log(self.file_object,'End of Prediction')
-            #print(result)
 
 
 
